5ba87ad37bc93756be6ef3c3680957218b34388f
Ankit Kumar <ankit0841439@gmail.com>
2025-05-05 17:15:04 +0530

    utills code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ import express from "express";
 const app = express();
 import { connectDB } from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(
+    () =>
+      app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+      }),
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
